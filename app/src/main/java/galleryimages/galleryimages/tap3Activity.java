1baf73c9e669172d91be24780493696f9939d323
LaLlorona <40677863+LaLlorona@users.noreply.github.com>
2018-07-02 20:57:01 +0900

    scratch_tab3_implementing
    custom_view_implementing

--- app/src/main/java/galleryimages/galleryimages/tap3Activity.java
+++ app/src/main/java/galleryimages/galleryimages/tap3Activity.java
@@ -1,10 +1,17 @@
 package galleryimages.galleryimages;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.List;
 
 public class tap3Activity extends AppCompatActivity {
 
@@ -38,6 +45,74 @@ public class tap3Activity extends AppCompatActivity {
             }
         });
 
+
+
+        CustomView customView = new CustomView(this);
+
+
+
+
+    }
+    public class CustomView extends View {
+        // '붓'에 해당하는 paint 클래스 변수 선언
+        private Paint paint;
+        private float x, y, r=5;
+
+        // 위치 값들을 저장하기 위한 List 생성
+        private List<Pos> data;
+
+        public CustomView(Context context) {
+            super(context);
+
+            // paint 클래스 선언 및 초기값 설정
+            paint = new Paint();
+            paint.setColor(Color.CYAN);
+            paint.setStrokeWidth(5f);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            // 저장된 포인트 그리기
+            for (Pos p : data) {
+                canvas.drawCircle(p.getX(), p.getY(), r, paint);
+            }
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            // 터치한 위치 값 추출
+            x = event.getX();
+            y = event.getY();
+
+            // Pos 클래스를 생성해 데이터 저장 후 List에 추가
+            Pos pos = new Pos(x, y);
+            data.add(pos);
+
+            // 화면을 강제로 그리기 위해 호출하는 메소드
+            invalidate();
+
+            return true;
+        }
+    }
+
+    /**
+     * X, Y 좌표를 저장하기 위한 Pos 클래스
+     */
+    class Pos {
+        float x, y;
+
+        Pos(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public float getX() {
+            return x;
+        }
+
+        public float getY() {
+            return y;
+        }
     }
 
 }
